41f90529fd882128ede61628eb23bfc6c68f352d
Pedro Rosa <profpedrorosa@gmail.com>
2025-09-03 19:23:17 -0300

    Botão Nova Conversa e Implementação dos Chats: Armazenamento Local das Mensagens

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,8 +27,103 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+@override
+class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = TextEditingController();
+  final List<String> _messages = [];
+  bool _isInitialScreen = true;
+  late SharedPreferences _prefs;
+  List<Map<String, dynamic>> _conversations = [];
+  int _currentConversationIndex = -1;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadConversations();
+  }
+
+  Future<void> _loadConversations() async {
+    _prefs = await SharedPreferences.getInstance();
+    final List<String> loadedConversations =
+        _prefs.getStringList('conversations') ?? [];
+    setState(() {
+      _conversations = loadedConversations
+          .map((json) => jsonDecode(json) as Map<String, dynamic>)
+          .toList();
+      if (_conversations.isNotEmpty) {
+        _currentConversationIndex = 0;
+        _messages.addAll(List<String>.from(_conversations[0]['messages']));
+        _isInitialScreen = _messages.isEmpty;
+      }
+    });
+  }
+
+  Future<void> _saveConversations() async {
+    final List<String> jsonConversations = _conversations
+        .map((conv) => jsonEncode(conv))
+        .toList();
+    await _prefs.setStringList('conversations', jsonConversations);
+  }
+
+  void _handleSubmitted(String text) {
+    if (text.trim().isEmpty) {
+      return;
+    }
+
+    if (_isInitialScreen) {
+      _startNewConversation();
+    }
+    _textController.clear();
+    setState(() {
+      _messages.add(text);
+      _isInitialScreen = false;
+      _conversations[_currentConversationIndex]['messages'].add(text);
+    });
+    _saveConversations();
+  }
+
+  void _clearChat() {
+    setState(() {
+      _messages.clear();
+      _isInitialScreen = true;
+      if (_currentConversationIndex != -1) {
+        _conversations.removeAt(_currentConversationIndex);
+        _currentConversationIndex = -1;
+      }
+    });
+    _saveConversations();
+  }
+
+  void _startNewConversation() {
+    final newConversation = {
+      'name': 'Conversa #${_conversations.length + 1}',
+      'messages': [],
+    };
+    setState(() {
+      _conversations.insert(0, newConversation);
+      _currentConversationIndex = 0;
+      _messages.clear();
+      _isInitialScreen = true;
+    });
+    _saveConversations();
+  }
+
+  void _loadConversation(int index) {
+    setState(() {
+      _currentConversationIndex = index;
+      _messages.clear();
+      _messages.addAll(List<String>.from(_conversations[index]['messages']));
+      _isInitialScreen = _messages.isEmpty;
+    });
+    Navigator.of(context).pop();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,7 +143,17 @@ class ChatScreen extends StatelessWidget {
                 ),
               ),
             ),
-            ListTile(),
+            ..._conversations.asMap().entries.map((entry) {
+              final index = entry.key;
+              final conv = entry.value;
+              return ListTile(
+                title: Text(
+                  conv['name'],
+                  style: const TextStyle(color: Colors.white),
+                ),
+                onTap: () => _loadConversation(index),
+              );
+            }),
           ],
         ),
       ),
@@ -55,16 +162,26 @@ class ChatScreen extends StatelessWidget {
         // Cor de fundo da AppBar
         iconTheme: const IconThemeData(color: Colors.white),
         elevation: 0,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/logo_darkmode.png',
-              width: 40,
-              height: 150,
-            ),
-            //TODO: Ajustar imagem
-          ],
+        title: Center(
+          child: Image.asset(
+            'assets/images/logo_darkmode.png',
+            width: 40,
+            //height: 150,
+          ),
+        ),
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              icon: const Icon(
+                Icons.menu_rounded,
+                color: Colors.white,
+                size: 28,
+              ),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          },
         ),
         actions: [
           /*
@@ -79,51 +196,89 @@ class ChatScreen extends StatelessWidget {
             },
           ),
           */
+          //Botão: Deletar conversa
           IconButton(
             icon: const Icon(
               Icons.delete_outline,
               color: Colors.white,
               size: 24,
             ),
-            onPressed: () {
-              // TODO: Limpar conversa
-            },
+            onPressed: _clearChat,
+          ),
+          // Botão: Nova Conversa
+          TextButton(
+            onPressed: _startNewConversation,
+            child: const Text(
+              'Nova Conversa',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
         ],
       ),
+
       body: Stack(
         children: [
-          // Conteúdo central
-          Positioned.fill(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Text(
-                  'Como posso ajudar?',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.white,
+          if (_isInitialScreen)
+            Positioned.fill(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Como posso ajudar?',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                // Botões de modo
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    _buildModeButton('Modo Inclusivo', true),
-                    const SizedBox(width: 10),
-                    _buildModeButton('Modo Libras', false),
-                  ],
-                ),
-                const SizedBox(height: 15),
-                // Botão "Comece a Conversar !"
-                _buildStartConversationButton(),
-              ],
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      _buildModeButton('Modo Inclusivo', true),
+                      const SizedBox(width: 10),
+                      _buildModeButton('Modo Libras', false),
+                    ],
+                  ),
+                  const SizedBox(height: 15),
+                  _buildStartConversationButton(),
+                ],
+              ),
+            )
+          else
+            ListView.builder(
+              padding: const EdgeInsets.only(
+                bottom: 80.0,
+                top: 10.0,
+                left: 16.0,
+                right: 16.0,
+              ),
+              reverse: false,
+              // Isso faz com que a lista role para baixo por padrão
+              itemCount: _messages.length,
+              itemBuilder: (context, index) {
+                final message = _messages[index];
+                return Align(
+                  alignment: Alignment.centerRight, // Mensagem do usuário
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(vertical: 4.0),
+                    padding: const EdgeInsets.all(12.0),
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF6B6BCF),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Text(
+                      message,
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  ),
+                );
+              },
             ),
-          ),
-          // Input fixo na parte inferior
           Align(
             alignment: Alignment.bottomCenter,
             child: Container(
@@ -131,17 +286,19 @@ class ChatScreen extends StatelessWidget {
                 horizontal: 16.0,
                 vertical: 8.0,
               ),
-              color: const Color(0xFF4C4C59), // Fundo do campo de entrada
+              color: const Color(0xFF4C4C59),
               child: Row(
                 children: [
-                  const Expanded(
+                  Expanded(
                     child: TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Pergunte ao CHAMBA-IA...',
+                      controller: _textController,
+                      onSubmitted: _handleSubmitted,
+                      decoration: const InputDecoration(
+                        hintText: 'Pergunte ao CHAMBA-AI...',
                         hintStyle: TextStyle(color: Color(0xFF8F8F99)),
                         border: InputBorder.none,
                       ),
-                      style: TextStyle(color: Colors.white),
+                      style: const TextStyle(color: Colors.white),
                     ),
                   ),
                   IconButton(
@@ -150,9 +307,7 @@ class ChatScreen extends StatelessWidget {
                       color: Color(0xFF6B6BCF),
                       size: 28,
                     ),
-                    onPressed: () {
-                      // TODO: Implementar envio da mensagem
-                    },
+                    onPressed: () => _handleSubmitted(_textController.text),
                   ),
                 ],
               ),
